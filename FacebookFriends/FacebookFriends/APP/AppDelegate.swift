b8ffba6ca369cdedb4e41d716c2dc65303a02f85
Murat Can ASLAN <muratcan.aslan@foreks.com>
2023-08-09 15:46:25 +0300

    [FF-2] Add navigation manager to control route

--- FacebookFriends/FacebookFriends/APP/AppDelegate.swift
+++ FacebookFriends/FacebookFriends/APP/AppDelegate.swift
@@ -13,11 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        let vc = LoginViewController()
-        vc.modalTransitionStyle = .crossDissolve
-        self.window = UIWindow(frame: UIScreen.main.bounds)
-        self.window?.rootViewController = vc
-        self.window?.makeKeyAndVisible()
+        NavigationManager.shared.showLogin()
         return true
     }
 }
--- FacebookFriends/FacebookFriends/Managers/NavigationManager.swift
+++ FacebookFriends/FacebookFriends/Managers/NavigationManager.swift
@@ -5,4 +5,36 @@
 //  Created by Murat Can ASLAN on 9.08.2023.
 //
 
-import Foundation
+import UIKit
+
+final class NavigationManager {
+    static let shared = NavigationManager()
+    
+    private init() { }
+    
+    func showLogin() {
+        let vc = LoginViewController()
+        show(vc)
+    }
+    
+    func showFriendList() {
+        let vc = UINavigationController(rootViewController: FriendListViewController())
+        show(vc)
+    }
+    
+    private func getCurrentWindow() -> UIWindow {
+        var window = UIApplication.shared.windows.filter {$0.isKeyWindow}.first
+        if window == nil {
+            window = UIWindow(frame: UIScreen.main.bounds)
+            window?.makeKeyAndVisible()
+        }
+        return window!
+    }
+    
+    private func show(_ vc: UIViewController) {
+        let window = getCurrentWindow()
+        window.rootViewController = vc
+        window.makeKeyAndVisible()
+        UIView.transition(with: window, duration: 0.2, options: .transitionCrossDissolve, animations: nil, completion: nil)
+    }
+}
